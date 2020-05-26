cbac24ff812a829d905fd9f8c28d2a27bfc3475d
arusu0629 <arusu0629@gmail.com>
2020-05-26 10:02:00 +0900

    Add DataStore target

---
+++ DataStore/DataStore.h
@@ -0,0 +1,19 @@
+//
+//  DataStore.h
+//  DataStore
+//
+//  Created by nakandakari on 2020/05/26.
+//  Copyright © 2020 nakandakari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStore.
+FOUNDATION_EXPORT double DataStoreVersionNumber;
+
+//! Project version string for DataStore.
+FOUNDATION_EXPORT const unsigned char DataStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStore/PublicHeader.h>
+
+
