340d181c45c8bace480c750ce89466d51ce16055
arusu0629 <arusu0629@gmail.com>
2020-05-26 09:49:20 +0900

    Initial project

---
+++ RoyaleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoyaleApp
+//
+//  Created by nakandakari on 2020/05/26.
+//  Copyright © 2020 nakandakari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
