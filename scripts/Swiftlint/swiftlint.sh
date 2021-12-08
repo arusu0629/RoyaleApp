eca86c64aa9a4289f44d7015df481f13d5e72642
arusu0629 <arusu0629@gmail.com>
2021-12-08 14:05:53 +0900

    swiftlintが終わらないのを修正

--- scripts/Swiftlint/swiftlint.sh
+++ scripts/Swiftlint/swiftlint.sh
@@ -1,6 +1,6 @@
 if which mint >/dev/null; then
-  xcrun --sdk macosx mint run swiftlint swiftlint autocorrect --format
-  xcrun --sdk macosx mint run swiftlint swiftlint
+  mint run swiftlint swiftlint --fix --format
+  mint run swiftlint swiftlint
 else
   echo "warning: Mint not installed, download from https://github.com/yonaskolb/Mint"
 fi
