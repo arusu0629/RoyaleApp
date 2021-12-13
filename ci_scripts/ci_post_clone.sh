d9e0098c31c38fff0181a1fe13608233a76d356a
arusu0629 <arusu0629@gmail.com>
2021-12-13 15:08:46 +0900

    carthage bootstrap 戻す

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,8 +5,7 @@ brew install mint
 mint bootstrap -m ../Mintfile --overwrite y
 
 # Setup Carthage
-#mint run Carthage carthage bootstrap --platform iOS --cache-builds --use-xcframeworks --project-directory ../
-mint run Carthage carthage checkout --project-directory ../
+mint run Carthage carthage bootstrap --platform iOS --cache-builds --use-xcframeworks --project-directory ../
 
 # Generate xcodeproj
 mint run xcodegen xcodegen generate -s ../project.yml
