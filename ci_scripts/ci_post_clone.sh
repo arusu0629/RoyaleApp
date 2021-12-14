83461cbd625113428cd8099d32d586947604b34f
arusu0629 <arusu0629@gmail.com>
2021-12-14 10:21:51 +0900

    copy-frameworksするタイミングをちょっと変更

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,8 @@ mint bootstrap -m ../Mintfile --overwrite y
 
 # Setup Carthage
 mint run Carthage carthage bootstrap --platform iOS --cache-builds --use-xcframeworks --project-directory ../
-mint run Carthage carthage update --platform iOS --cache-builds --use-xcframeworks --project-directory ../
+#mint run Carthage carthage update --platform iOS --cache-builds --use-xcframeworks --project-directory ../
+mint run Carthage carthage copy-frameworks
 
 # Generate xcodeproj
 mint run xcodegen xcodegen generate -s ../project.yml
