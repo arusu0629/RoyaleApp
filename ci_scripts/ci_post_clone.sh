0357c74fd9a47cbef627244ab9375daef72fa6dd
arusu0629 <arusu0629@gmail.com>
2021-12-15 16:54:39 +0900

    再度 carthage update を削除

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,6 @@ mint bootstrap -m ../Mintfile --overwrite y
 
 # Setup Carthage
 mint run Carthage carthage bootstrap --platform iOS --cache-builds --use-xcframeworks --project-directory ../
-mint run Carthage carthage update --platform iOS --cache-builds --use-xcframeworks --project-directory ../
 
 # Generate xcodeproj
 mint run xcodegen xcodegen generate -s ../project.yml
